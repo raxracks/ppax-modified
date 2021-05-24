075a6bdcd75dd08382ca61c6927b9f216412742e
raxracks <multisniperism@gmail.com>
2021-05-25 10:33:23 +1200

    did literally nothing

--- srcs/entry/init.cpp
+++ srcs/entry/init.cpp
@@ -12,50 +12,6 @@ VideoGraphicsArray *vgap;
 
 uint32_t buffer[width * height]; // hard coded - not good please change
 
-int sqrt(int x) {
-  // let initial guess to be 1
-  int z = 1.0;
-  for(int i = 1; i <= 10; i++) {
-    z -= (z*z - x) / (2*z); // MAGIC LINE!!
-  }
-  return z;
-}
-
-void circleSimple(int xCenter, int yCenter, int radius, uint32_t c)
-    {
-        int pix = c;
-        int x, y, r2;
-        
-        r2 = radius * radius;
-        for (x = -radius; x <= radius; x++) {
-            y = (int)(sqrt(r2 - x*x) + 0.5);
-            vgap->PutPixel(xCenter + x, yCenter + y, pix);
-            vgap->PutPixel(xCenter + x, yCenter - y, pix);
-        }
-    }
-
-void RoundWindow(int32_t x, int32_t y, uint32_t w, uint32_t h) { // create a window
-	
-
-	/*vgap->FillRectangle(x, y, w, h, 0x909090); // border
-	vgap->FillRectangle(x+2, y+2, w-4, 2, 0x2040E0); // top bar
-	vgap->FillRectangle(x+2, y+4, w-4, 8, 0x1030A0); // top bar
-	vgap->FillRectangle(x+2, y+12, w-4, 2, 0x002080); // top bar
-	vgap->FillRectangle(x+2, y+14, w-4, h-28, 0xE0E0E0); // content
-	vgap->FillRectangle(x+2, y+h-12, w-4, 10, 0xC0C0C0); // bottom bar
-	vgap->FillRectangle(x+w-37, y+3, 10, 10, 0xD0D0D0); // button back
-	vgap->FillRectangle(x+w-25, y+3, 10, 10, 0xD0D0D0); // button back
-	vgap->FillRectangle(x+w-13, y+3, 10, 10, 0xD0D0D0); // button back
-	vgap->PutStr("chados", x + 4, y + 5, 0xFFFFFF);
-
-	vgap->PutChar('_', x+w-36, y+4, 0x101010);
-	vgap->FillRectangle(x+w-24, y+4, 8, 8, 0x101010);
-	vgap->FillRectangle(x+w-23, y+5, 6, 6, 0xD0D0D0);
-	vgap->PutChar('X', x+w-12, y+4, 0x101010);
-
-	vgap->PutStr("i totally wrote all the code for this", x + 4, y + 20, 0x000000);*/
-}
-
 void window(int32_t x, int32_t y, uint32_t w, uint32_t h) { // create a window
 
 	vgap->FillRectangle(x, y, w, h, 0x909090); // border
@@ -74,7 +30,9 @@ void window(int32_t x, int32_t y, uint32_t w, uint32_t h) { // create a window
 	vgap->FillRectangle(x+w-23, y+5, 6, 6, 0xD0D0D0);
 	vgap->PutChar('X', x+w-12, y+4, 0x101010);
 
-	vgap->PutStr("i totally wrote all the code for this", x + 4, y + 20, 0x000000);
+	vgap->PutStr("hello", x + 4, y + 20, 0x000000);
+
+	vgap->PutStr("circle", x + 4, y + 30, 0x000000);
 }
 
 char* itoa(int value, int base = 10) // matei
@@ -117,21 +75,41 @@ char* itoa(int value, int base = 10) // matei
     return rc;
 }
 
-void HandleMessage(char* message) {
-	
-}
+int x = 20;
+	int y = 0;
+	int i = 0;
+
+	int windowX = 0;
+	int windowY = 0;
+
+	void HandleMessage(char* message) {
+		if(strcmp(message, "createwindow") == 1) {
+			window(windowX, windowY, 500, 500);
+		} else if(strcmp(message, "right") == 1) {
+			windowX += 10;
+		} else if(strcmp(message, "left") == 1) {
+			windowX -= 10;
+		} else if(strcmp(message, "up") == 1) {
+			windowY -= 10;
+		} else if(strcmp(message, "down") == 1) {
+			windowY += 10;
+		} else if(strcmp(message, "clear") == 1) {
+			vgap->Clear();
+
+			x = 20;
+			y = 0;
+		}
+	}
 
 extern "C" void kernelMain(uint32_t stackPointer, const multiboot_header* multiboot_structure, uint32_t /*multiboot_magic*/) {
 	VideoGraphicsArray vga(multiboot_structure, buffer);
 
 	vgap = &vga;
 
-	int x = 20;
-	int y = 0;
-	int i = 0;
+	
 
 	while(true) {
-		vgap->Clear();
+		//vgap->Clear();
 
 		char string[1000]; // define command array
 
@@ -139,9 +117,9 @@ extern "C" void kernelMain(uint32_t stackPointer, const multiboot_header* multib
 
 		char character = nonblocking_getchar(); // get character
 
-		//vgap->FillRectangle(x, y + 8, 10, 2, 0xFFFFFF); // cursor
+		vgap->FillRectangle(x, y + 8, 10, 2, 0xFFFFFF); // cursor
 
-		//vgap->FillRectangle(x - 10, y + 8, 10, 2, 0x0); // remove previous cursor
+		vgap->FillRectangle(x - 10, y + 8, 10, 2, 0x0); // remove previous cursor
 
 		if(character != 0) { // many hardcoded values below, very bad and ugly
 			if(character == 0x08) {
@@ -152,6 +130,8 @@ extern "C" void kernelMain(uint32_t stackPointer, const multiboot_header* multib
 				vgap->FillRectangle(x, y, 10, 10, 0x0000000);
 				vgap->FillRectangle(x + 10, y, 10, 10, 0x0000000);
 			} else if(character == 0x0D) {
+				vgap->FillRectangle(x, y + 8, 10, 2, 0x0); 
+
 				i = 0;
 				x = 20;
 				y += 10;
@@ -163,7 +143,7 @@ extern "C" void kernelMain(uint32_t stackPointer, const multiboot_header* multib
 				string[i] = character;
 				vgap->PutStr(&character, x, y, 0xFFFFFF);
 				x += 10;
-				if(x > 1024) {
+				if(x > width) {
 					x = 20;
 					y += 10;
 				}
@@ -174,7 +154,7 @@ extern "C" void kernelMain(uint32_t stackPointer, const multiboot_header* multib
 			
 		}
 
-		circleSimple(x, 300, 50, 0xE0E0E0);
+		//circleSimple(x, 300, 50, 0xE0E0E0);
 
 		vga.SwapBuffers();
 	}
